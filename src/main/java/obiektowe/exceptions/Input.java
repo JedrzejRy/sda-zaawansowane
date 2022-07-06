5fd6374ab3fdd76b9e9c4b000d498380000ae78d
Jedrzej Rynecki <metrexaxa@gmail.com>
2022-07-06 19:14:16 +0200

    InputMismatchException task

---
+++ src/main/java/obiektowe/exceptions/Input.java
@@ -0,0 +1,30 @@
+package obiektowe.exceptions;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Input {
+
+    public int readNumber() {
+        System.out.print("Podaj Numer: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        boolean test = true;
+
+        while (test) {
+            try {
+                test = false;
+                int number = input.readNumber();
+            } catch (InputMismatchException e) {
+                System.out.println("Zla zmienna!\n Podaj Numer:");
+                test = true;
+            }
+
+        }
+    }
+
+}
